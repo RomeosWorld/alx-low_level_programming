408c4f1fa2c1bf2c6d8ce8569b86083062df62d5
RomeosWorld <slimgohd@gmail.com>
2022-12-08 18:29:34 -0800

    puts task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: 'this ia a learning task'
- * Return: Always 0 (Success)
+ * Description: First C task
+ * Return: Always 0
  */
-
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
