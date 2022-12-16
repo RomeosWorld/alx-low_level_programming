feb38dce133c7c8ebbc372c526863f8d601d9671
RomeosWorld <slimgohd@gmail.com>
2022-12-15 18:45:15 -0800

    tasks

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * mul - Multiplies two integers
+  * @a: The first number to multiply
+  * @b: The second number to multiply with first number
+  * Return: The result of multiplication between a and b
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
