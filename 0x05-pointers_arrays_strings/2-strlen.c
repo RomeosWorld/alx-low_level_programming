811e4e0ca6466fef33a184751c425cde80a90d89
RomeosWorld <slimgohd@gmail.com>
2022-12-20 08:23:36 -0800

    strlen task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the string of a string
+ * @s: string
+ * Return: Length
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (length)
+}
