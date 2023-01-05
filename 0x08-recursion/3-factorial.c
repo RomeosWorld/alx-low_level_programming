4c57fd85663c7c2e6a75af3d3164c15fad9a5d46
RomeosWorld <slimgohd@gmail.com>
2023-01-05 07:16:45 -0800

    alx tasks

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main."
+
+/**
+ * factiorial - returns a factorial of an integer
+ * @n: Given number
+ * Return: Factorial of n
+ */
+int factorial(int n);
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+		return (1);
+	return (n * factorial(n -1));
+}
